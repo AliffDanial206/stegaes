5ca9e7341cc639ccbd4f200ef2744c762c7f3c81
AliffDanial206 <85630777+AliffDanial206@users.noreply.github.com>
2023-03-04 09:18:33 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Coding;
+
+/**
+ *
+ * @author USER
+ */
+public class Main {
+    public static void main(String[] args){
+        Encode_Frame ef = new Encode_Frame();
+        ef.setVisible(true);
+        ef.setLocationRelativeTo(null);
+    }
+    
+}
